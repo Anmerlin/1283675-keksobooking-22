a1039dcb7886b0b858e664eb3b78b67d9c4dea36
Mirddin Emris <anmerlin@mail.ru>
2021-02-16 01:18:02 +0300

    Подготавливает код для генгерации разметки похожих объявлений на основе данных

--- js/data.js
+++ js/data.js
@@ -4,12 +4,42 @@ import {
   getArrayRandomLengthFromValues
 } from './util.js';
 
-const TITLE_OFFER = ['Superior apartment', 'Sweet home', 'Luxurious palace', 'Open bungalow'];
-const TYPE_OFFER = ['palace', 'flat', 'house', 'bungalow'];
-const CHECKIN_CHECKOUT_OFFER = ['12:00', '13:00', '14:00'];
-const FEATURES_OFFER = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const DESCRIPTION_OFFER = ['Superior apartment overlooking the park', 'A spacious palace where you can get lost', 'Two-storey house with vegetable garden and garage', 'Bungalow blown by all the winds'];
-const PHOTOS_OFFER = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const TITLE_OFFER = [
+  'Superior apartment',
+  'Sweet home',
+  'Luxurious palace',
+  'Open bungalow',
+];
+const TYPE_OFFER = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+const CHECKIN_CHECKOUT_OFFER = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES_OFFER = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTION_OFFER = [
+  'Superior apartment overlooking the park',
+  'A spacious palace where you can get lost',
+  'Two-storey house with vegetable garden and garage',
+  'Bungalow blown by all the winds',
+];
+const PHOTOS_OFFER = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
 
 /**
  * Function of creating an ad object announcement
@@ -29,8 +59,8 @@ const createAnnouncement = () => {
       address: addressValue, // строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
       price: getRandomNumberRangeInclusive(50, 500), // число — стоимость. Любое положительное число.
       type: getRandomElementFromArray(TYPE_OFFER), //строка — одно из четырёх фиксированных значений: palace, flat, house или bungalow.
-      rooms: getRandomNumberRangeInclusive(2, 10), //число — количество комнат. Любое положительное число.
-      guests: getRandomNumberRangeInclusive(6, 20), // число — количество гостей, которое можно разместить. Любое положительное число.
+      rooms: getRandomNumberRangeInclusive(1, 11), //число — количество комнат. Любое положительное число.
+      guests: getRandomNumberRangeInclusive(1, 21), // число — количество гостей, которое можно разместить. Любое положительное число.
       checkin: getRandomElementFromArray(CHECKIN_CHECKOUT_OFFER), // строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
       checkout: getRandomElementFromArray(CHECKIN_CHECKOUT_OFFER), // строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
       features: getArrayRandomLengthFromValues(FEATURES_OFFER), // массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
@@ -44,6 +74,20 @@ const createAnnouncement = () => {
   };
 };
 
+
+//const similarAnnouncements = new Array(SIMILAR_ANNOUNCEMENTS_COUNT).fill(null).map(() => createaAnnouncement());
+/**
+ * @param  {number} count
+ * @return  {Array} {announcements[i]=createAnnouncement(
+ */
+const similarAnnouncements = (count) => {
+  let announcements = [];
+  for (let i = 0; i < count; i++) {
+    announcements[i] = createAnnouncement();
+  }
+  return announcements;
+}
+
 export {
-  createAnnouncement
+  similarAnnouncements
 };
--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-import {
-  createAnnouncement
-} from './data.js';
-
-const SIMILAR_ANNOUNCEMENTS_COUNT = 10;
-
-//const similarAnnouncements = new Array(SIMILAR_ANNOUNCEMENTS_COUNT).fill(null).map(() => createaAnnouncement());
-
-const similarAnnouncements = (count) => {
-  let announcements = [];
-  for (let i = 0; i < count; i++) {
-    announcements[i] = createAnnouncement();
-  }
-  return announcements;
-}
-
-similarAnnouncements(SIMILAR_ANNOUNCEMENTS_COUNT);
+import './card.js';
